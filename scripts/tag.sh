6f2d9d464f652bdf0bc50e350f443707634d5f5c
Matt Travi <programmer@travi.org>
2017-09-24 22:08:17 -0500

    ci(git-tag): moved the git config outside of the conditional

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+git config --global user.email "$GIT_EMAIL"
+git config --global user.name "$GIT_NAME"
+
 if ! git rev-parse v"$(jq '.dependencies."hapi-greenkeeper-keeper"' -r package.json)" >/dev/null 2>&1; then
-  git config --global user.email "$GIT_EMAIL"
-  git config --global user.name "$GIT_NAME"
   git tag v"$(jq '.dependencies."hapi-greenkeeper-keeper"' -r package.json)"
   git push --tags "https://$GH_TOKEN@github.com/greenkeeper-keeper/reference-instance" >/dev/null 2>&1
 else
