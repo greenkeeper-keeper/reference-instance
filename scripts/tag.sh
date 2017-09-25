cc728912118bdcc501edcb6b915f7455bac70a38
Matt Travi <programmer@travi.org>
2017-09-24 22:40:42 -0500

    ci(git-tag): removed the quotes from the url for the remote repo

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -5,7 +5,7 @@ git config --global user.name "$GIT_NAME"
 
 if ! git rev-parse v"$(jq '.dependencies."hapi-greenkeeper-keeper"' -r package.json)" >/dev/null 2>&1; then
   git tag v"$(jq '.dependencies."hapi-greenkeeper-keeper"' -r package.json)"
-  git push --tags "https://$GH_TOKEN@github.com/greenkeeper-keeper/reference-instance"
+  git push --tags https://$GH_TOKEN@github.com/greenkeeper-keeper/reference-instance
 else
   echo "tag already exists"
 fi
